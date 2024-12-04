84b7c2adac5a86ab88483dae6f97a3769ea5eb77
Kazuki Hashimoto <kazukih0205@gmail.com>
2024-12-04 19:43:59 +0000

    wlan: qcacld-3.0: Disable PM QoS
    This gets held for a really long time.

--- qcom/opensource/wlan/qcacld-3.0/core/pld/src/pld_common.c
+++ qcom/opensource/wlan/qcacld-3.0/core/pld/src/pld_common.c
@@ -799,7 +799,6 @@ void pld_request_pm_qos(struct device *dev, u32 qos_val)
 {
 	switch (pld_get_bus_type(dev)) {
 	case PLD_BUS_TYPE_PCIE:
-		pld_pcie_request_pm_qos(dev, qos_val);
 		break;
 	case PLD_BUS_TYPE_PCIE_FW_SIM:
 	case PLD_BUS_TYPE_IPCI_FW_SIM:
@@ -823,7 +822,6 @@ void pld_remove_pm_qos(struct device *dev)
 {
 	switch (pld_get_bus_type(dev)) {
 	case PLD_BUS_TYPE_PCIE:
-		pld_pcie_remove_pm_qos(dev);
 		break;
 	case PLD_BUS_TYPE_PCIE_FW_SIM:
 	case PLD_BUS_TYPE_IPCI_FW_SIM:
