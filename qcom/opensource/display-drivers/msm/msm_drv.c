17d9fbeeee48083a5cf8038ae04c6a6cbbe7b850
Park Ju Hyung <qkrwngud825@gmail.com>
2025-01-18 11:41:57 +0000

    display-drivers: msm: sde: Allocate kmem_fence_pool once
    sde_fence_init() is called multiple times without matching
    sde_fence_deinit() calls, resulting in memory leaks and mismatched
    allocs and frees.
    
    Just allocate the kmem pool once.
    
    Fixes: 49b09fc235c8 ("drm/msm/sde: use kmem_cache pool for struct sde_fence")
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- qcom/opensource/display-drivers/msm/msm_drv.c
+++ qcom/opensource/display-drivers/msm/msm_drv.c
@@ -58,6 +58,7 @@
 #include "msm_mmu.h"
 #include "sde_wb.h"
 #include "sde_dbg.h"
+#include "sde_fence.h"
 
 /*
  * MSM driver version:
@@ -2354,6 +2355,7 @@ static int __init msm_drm_register(void)
 		return -EINVAL;
 
 	DBG("init");
+        sde_kmem_pool_init();
 	sde_rsc_rpmh_register();
 	sde_rsc_register();
 	msm_smmu_driver_init();
